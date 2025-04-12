1242ac28114f3e10eea7ff77fa24fd87a9d5403e
Hannah Casier <hannah.casier@me.com>
2025-04-12 14:37:38 +0200

    edit cookies en cors config

--- src/server.ts
+++ src/server.ts
@@ -18,10 +18,11 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: "https://boostmeup.surge.sh",
+    origin: ["https://boostmeup.surge.sh", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Accept", "Authorization"],
+    exposedHeaders: ["set-cookie"],
   })
 );
 app.use(express.json());
