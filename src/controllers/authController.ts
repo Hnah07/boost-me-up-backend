ec78c6165f05f19f38b294d872e99480d7e9de1b
Hannah Casier <hannah.casier@me.com>
2025-04-12 14:26:14 +0200

    modified secure to true

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -40,7 +40,7 @@ export const register = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production" ? true : false,
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
