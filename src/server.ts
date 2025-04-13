74364191fcb994c5e6dfc9679cc4dc1c5ec9f5f4
Hannah Casier <hannah.casier@me.com>
2025-04-13 17:23:40 +0200

    edit origins

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,11 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: ["https://boostmeup.surge.sh", "http://localhost:5173"],
+    origin: [
+      "https://boostmeup.hannahc.be",
+      "http://boostmeup.hannahc.be",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Accept", "Authorization"],
