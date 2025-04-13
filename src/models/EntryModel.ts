dff62b1b3992208df68b0b2aa1d4af2f746d6b31
Hannah Casier <hannah.casier@me.com>
2025-04-13 22:58:34 +0200

    simple entry save

--- src/models/EntryModel.ts
+++ src/models/EntryModel.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
 interface IEntry extends mongoose.Document {
   content: string;
@@ -7,7 +6,6 @@ interface IEntry extends mongoose.Document {
   isPrivate: boolean;
   createdAt: Date;
   updatedAt: Date;
-  _originalContent?: string;
 }
 
 const entrySchema = new mongoose.Schema<IEntry>(
@@ -28,52 +26,11 @@ const entrySchema = new mongoose.Schema<IEntry>(
   },
   {
     timestamps: true,
-    toJSON: {
-      transform: function (doc, ret) {
-        if (doc._originalContent) {
-          ret.content = doc._originalContent;
-        }
-        return ret;
-      },
-    },
-    toObject: {
-      transform: function (doc, ret) {
-        if (doc._originalContent) {
-          ret.content = doc._originalContent;
-        }
-        return ret;
-      },
-    },
   }
 );
 
 // Add an index to optimize queries by user
 entrySchema.index({ user: 1 });
-
-// Pre-save middleware to hash the content
-entrySchema.pre("save", function (next) {
-  if (this.isModified("content")) {
-    const originalContent = this.content;
-    this._originalContent = originalContent;
-    this.content = bcrypt.hashSync(originalContent, 10);
-  }
-  next();
-});
-
-// Post-find middleware to restore original content
-entrySchema.post("find", function (docs) {
-  docs.forEach((doc: any) => {
-    if (doc._originalContent) {
-      doc.content = doc._originalContent;
-    }
-  });
-});
-
-entrySchema.post("findOne", function (doc: any) {
-  if (doc && doc._originalContent) {
-    doc.content = doc._originalContent;
-  }
-});
 
 const Entry = mongoose.model<IEntry>("Entry", entrySchema);
 
